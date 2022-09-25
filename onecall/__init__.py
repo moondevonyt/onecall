005aa4eeffaefce7bbd6885d351809374f393d68
joshyjoy <joshyjoy999@gmail.com>
2022-09-25 21:20:18 +0530

    init updated

--- onecall/__init__.py
+++ onecall/__init__.py
@@ -1,3 +1,10 @@
+from binance import Binance
+from phemex import Phemex
+from kucoin import Kucoin
+from bybit import Bybit
+from ftx import FTX
+from ftx_us import FTXUS
+
 exchanges = [
     'Biance',
     'Phemex',

ebb7834484f8ec423b24919e15a05956d4661a0d
joshyjoy <joshyjoy999@gmail.com>
2022-09-25 17:53:09 +0530

    BASE API CLASS COMPLETED

--- onecall/base/exchange.py
+++ onecall/base/exchange.py
@@ -7,7 +7,9 @@ from .exceptions import ClientException, ServerException
 
 
 class Exchange:
-
+    """
+    API Basic class
+    """
     def __init__(
             self,
             key=None,
@@ -16,6 +18,15 @@ class Exchange:
             base_url=None,
             show_limit_usage=False,
     ):
+        """
+        Initialise client
+
+        :param key: API key
+        :param secret: secret key
+        :param pass_phrase: pass phrase for kucoin
+        :param base_url: base url
+        :param show_limit_usage: flag to show usage
+        """
         self.key = key
         self.secret = secret
         self.pass_phrase = pass_phrase
@@ -32,6 +43,16 @@ class Exchange:
         return
 
     def send_request(self, http_method, url_path, header=None, params=None, data=None):
+        """
+        function to create and send HTTP request
+
+        :param http_method: http method
+        :param url_path: api uri path
+        :param header: request header
+        :param params: request query parameters
+        :param data: request json data
+        :return: http response
+        """
         try:
             url = self.base_url + url_path
             self._logger.debug("url: " + url)
@@ -49,6 +70,12 @@ class Exchange:
             return self._send_error_response(e)
 
     def _dispatch_request(self, http_method):
+        """
+        function to get http dispatch method
+
+        :param http_method: http method
+        :return: http method
+        """
         return {
             "GET": self.session.get,
             "DELETE": self.session.delete,
